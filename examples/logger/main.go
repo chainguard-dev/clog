b4b0b2304532d6f51eb9a329c94f7f3fab7f2cd9
Jason Hall <jason@chainguard.dev>
2024-02-02 16:44:26 -0500

    WIP: add a flag option
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- examples/logger/main.go
+++ examples/logger/main.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log/slog"
+	"os"
 
 	"github.com/chainguard-dev/clog"
 )
 
 func main() {
+	level := clog.LevelFlag("log-level", slog.LevelInfo, "log level")
+	flag.Parse()
+
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: slog.Level(level.Get()),
+	})))
+
 	log := clog.NewLogger(slog.Default()).With("a", "b")
 	ctx := clog.WithLogger(context.Background(), log)
 
 	// Grab logger from context and use
 	clog.FromContext(ctx).With("foo", "bar").Infof("hello world")
+
+	clog.FromContext(ctx).With("foo", "bar").Debugf("hello debug world")
 
 	// Package level context loggers are also aware
 	clog.ErrorContext(ctx, "asdf")
