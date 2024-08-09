d36bc967c6d6f1bf5445da1e452440833a2033c2
Jason Hall <jason@chainguard.dev>
2024-08-09 10:46:31 -0400

    slogtest: include debug logs by default (#23)
    https://github.com/chainguard-dev/clog/pull/13#discussion_r1711599434
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- slogtest/slogtest_test.go
+++ slogtest/slogtest_test.go
@@ -13,4 +13,9 @@ func TestSlogTest(t *testing.T) {
 	clog.FromContext(ctx).With("foo", "bar").Infof("hello world")
 	clog.FromContext(ctx).With("bar", "baz").Infof("me again")
 	clog.FromContext(ctx).With("baz", true).Infof("okay last one")
+
+	clog.FromContext(ctx).Debug("hello debug")
+	clog.FromContext(ctx).Info("hello info")
+	clog.FromContext(ctx).Warn("hello warn")
+	clog.FromContext(ctx).Error("hello error")
 }
